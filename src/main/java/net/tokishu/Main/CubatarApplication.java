75e01b17fc7a3727e26388ea4909bf5ca6982927
Tokishu <arch.wizard.megumi@gmail.com>
2026-01-09 14:54:05 +0100

    Init

---
+++ src/main/java/net/tokishu/Main/CubatarApplication.java
@@ -0,0 +1,13 @@
+package net.tokishu.Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubatarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CubatarApplication.class, args);
+    }
+
+}
